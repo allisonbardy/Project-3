b82a9f5ced9228b4ee44d4a856418e748af6582c
Justin Ramos <jmramos396@gmail.com>
2019-06-20 15:44:26 -0400

    update

--- client/src/Components/Searchbar/Search.js
+++ client/src/Components/Searchbar/Search.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Search.css';
 
-const search = () => {
+const search = (props) => {
    return (
     <div className="body">
       <h1 className="title"> SEARCH FOR A STOCK </h1>
-      <input className="search" placeholder = "SEARCH..."/>
-      <button className="submit"> SUBMIT </button>
+      <input type="text"  name="symbol" value={props.symbol} onChange={props.change}/>
+      <button onClick={props.submit}> SUBMIT </button>
+      {props.children}
     </div>
     
    )
