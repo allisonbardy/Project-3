2a4f37a34b33e7167770e805be7748b308f9a5d3
Justin Ramos <jmramos396@gmail.com>
2019-06-26 23:28:43 -0400

    row

--- client/src/Components/Portfolio/portfolioRow.js
+++ client/src/Components/Portfolio/portfolioRow.js
@@ -1,37 +1,26 @@
 import React from "react"
 import "./Portfolio.css"
 
-
-
-
 export default function PortfolioRow(props) {
-    console.log(props.symbol, props.dividend)
-   
-    return (                
-                 <tbody>
-                 <tr>
-                    
-                     <td>{props.symbol}</td>
-                     <td>{props.close}</td>
-                     <td>{props.open}</td>
-                     <td>{props.latestPrice}</td>
-                     <td>{props.change}</td>
-                     {props.dividend && props.dividend.divAmount ? (
-                         <td>{props.dividend.divAmount}</td>
-                     ):
-                     (
-                        <td>N/A</td>
-                     )
-                     
-                    } 
-                    <td>{props.volume}</td>       
-                     {/* <td><button onClick={() => props.ownOnClick(props.index)}></button></td>
-                     <td><button onClick={() => props.watchOnClick(props.index)}></button></td> */}
 
-                 </tr>
-             </tbody>
-             
-            )
-           
-    
-          }              
+    return (
+        <tbody>
+            <tr>
+                <td>{props.symbol}</td>
+                <td>{props.close}</td>
+                <td>{props.open}</td>
+                <td>{props.latestPrice}</td>
+                <td>{props.change}</td>
+                {props.dividends ? (
+                    <td>{props.dividends}</td>
+                ) :
+                    (
+                        <td>N/A</td>
+                    )
+
+                }
+                <td>{props.volume}</td>
+            </tr>
+        </tbody>
+    )
+}              
