0221d64d8b9f0337162334ed6c3c71cf1348d482
Justin Ramos <jmramos396@gmail.com>
2019-06-26 23:25:29 -0400

    porthead

--- client/src/Components/Portfolio/PortfolioHead.js
+++ client/src/Components/Portfolio/PortfolioHead.js
@@ -9,6 +9,7 @@ export default function PortfolioHead(props) {
          <table className="table">
             <thead>
                <tr>
+                  <th className ="titles" scope="col">Stock Name</th>
                   <th className ="titles" scope="col">Close</th>
                   <th className ="titles" scope="col">Open</th>
                   <th className ="titles" scope="col">Latest Price</th>
@@ -16,7 +17,9 @@ export default function PortfolioHead(props) {
                   <th className ="titles" scope="col">Dividends</th>
                   <th className ="titles" scope="col">Volume</th>
                </tr>
-               </thead>
-               </table>
-               </div>
-   )}
+            </thead>
+            {props.children}
+         </table>
+      </div>
+   )
+}
